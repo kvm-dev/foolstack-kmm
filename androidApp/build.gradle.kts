babe07868180eadb8b771c7d1041ddfbb55ff5b8
pr0r0ck <mastadji@gmail.com>
2025-04-09 11:15:33 +0300

    ui fixes

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "ru.foolstack.foolstack.android"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
+        versionCode = 2
         versionName = version
         buildConfigField("String", "VERSION_NAME", properties["VERSION_NAME"].toString())
     }
